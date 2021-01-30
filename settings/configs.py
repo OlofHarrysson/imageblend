41dd348eb5fdf7cc086050e14e105b242d3c50aa
Olof Harrysson <harrysson.olof@gmail.com>
2021-01-30 17:33:46 +0100

    commit

--- settings/configs.py
+++ settings/configs.py
@@ -62,7 +62,7 @@ class TrainingConfig():
     # Misc configs
     self.misc = MiscConfig()
 
-    self.style_loss_weight = 1e3
+    self.style_loss_weight = 1e4
     self.content_loss_weight = 1
 
 

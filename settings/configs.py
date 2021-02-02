00f4fc368a2964c9a71088294a305c41ced0c57b
Olof Harrysson <harrysson.olof@gmail.com>
2021-02-02 20:24:15 +0100

    commit

--- settings/configs.py
+++ settings/configs.py
@@ -81,7 +81,7 @@ class TrainingConfig():
     # -1 equals the raw-styled image
     # 37 is avg_pooling
     self.style_layers = [3, 8, 18, 22, 26, 35]
-    self.content_layers = [37]
+    self.content_layers = [35]
     self.styled_content_layers = set(self.style_layers + self.content_layers)
 
 
